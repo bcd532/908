353fcf62c69c5f91f4efe41bf62e74f5119bbf6b
bcd532-laptop <bcd532@outerspace.com>
2026-07-04 22:11:26 -0400

    Added print messages to kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,19 +1,38 @@
 bits 16
 org 0x0000
 
+%define ENDL 0x0D, 0X0A
+
 start:
     cli
-    call puts_print 
+    mov ax, 0x07E0
+    mov ds, ax
 
+    mov si, k_msg
+    call puts
 
-puts_print:
+     
+
+puts:
+    push si 
+    push ax
+
+.puts_loop
+    lodsb
+    or al, al 
+    jz .putsp_done
+
     mov ah, 0x0E
-    mov al, 'K'
-    mov bh, 0
+    mov bh, 0 
     int 0x10
+    jmp .puts_loop
 
-    hlt
+.putsp_done:
+    pop ax
+    pop si
+  
 
+k_msg: db 'Hey', ENDL, 0
 
 .halt:
     hlt
