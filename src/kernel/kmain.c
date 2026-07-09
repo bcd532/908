a336ca5ef9a1dd994c2dc4cfb2196a55faf88a4d
bcd532-laptop <bcd532@outerspace.com>
2026-07-08 22:37:58 -0400

    kmain: console init + sleep boot messages

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include "console.h"
-
+#include "std9.h"
 
 #define VGA_BUFFER ((volatile uint16_t *)0xb8000)
 
@@ -9,6 +9,10 @@
 void kmain(void) {
     console_init();
     console_write("[ok] Kernel Loaded...");
+    sleep_ms(1000);
+    console_clear();
+    console_set_cursor(1,0);
+    console_write("Welcome to 908!");
 
     for (;;){}
 }
