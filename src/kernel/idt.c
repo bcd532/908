b70949fc798f0a598e7e77b7f736540dea6d4e25
bcd532-laptop <bcd532@outerspace.com>
2026-07-08 22:37:58 -0400

    idt: gate descriptor struct, set_descriptor and init

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -9,19 +9,26 @@ typedef struct {
 static idtr_t idtr;
 __attribute__((aligned(0x10)))
 
-static idt_entry_t idt[256]; // create an array of idt entries
+struct idt_entry_t idt[256]; // create an array of idt entries
 
 void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags);
 void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags){
-    idt_entry_t* descriptor = &idt[vector];
+    struct idt_entry_t* descriptor = &idt[vector];
 
     descriptor->isr            = (uint32_t)isr & 0xFFFF;
     descriptor->kernel_cs      = 0x08;
     descriptor->ist            = 0;
-    descriptor->attributes     = 0x8E;
-descriptor->isr_offset         = (isr >> 16) & 0xFFFF;
+    descriptor->attributes     = flags;
+    descriptor->isr_offset     = ((uint32_t)isr >> 16)& 0xFFFF;
 }
 
 
+
+void idt_init(){
+    idtr.limit = sizeof(idt) -1;
+    idtr.base = (uint32_t)&idt;
+    __asm__ volatile("lidt %0" : : ""(idtr));
+    
+}
 
 
--- src/kernel/idt.h
+++ src/kernel/idt.h
@@ -3,6 +3,15 @@
 
 #include <stdint.h>
 
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags);
+
+struct idt_entry_t{
+    uint16_t isr;
+    uint16_t kernel_cs;
+    uint8_t ist;
+    uint8_t attributes;
+    uint16_t isr_offset;
+} __attribute__((packed));
 
 
 #endif
