f80ea09ebe3a6c4eb530ffba589162b4968dc79e
bcd532-laptop <bcd532@outerspace.com>
2026-07-10 11:30:57 -0400

    itoa.c: updated comments
    updated comments to better understand things

--- src/kernel/itoa.c
+++ src/kernel/itoa.c
@@ -2,31 +2,39 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// unint to ascii
+/* convert an unsigned integer into a string using a given base
+ * (e.g) utoa(908, buf, 10);  <- will push out 908 using base 10 */
 void utoa(uint32_t val, char *buf, int base){   
-
     int32_t i = 0;
     int32_t left = 0;
+
     while(val != 0){
-        uint32_t remainder = val % base;
-        val = val / base;
-        buf[i] = "0123456789abcdef"[remainder];
+        uint32_t remainder = val % base;            /* put the remainder divided by the base into a temp var */
+        val = val / base;                           /* make the given value now equal the quotient */
+        buf[i] = "0123456789abcdef"[remainder];     /* a string lookup to find what the remainder equals to in asci (chars for addresses )
+                                                            [a haha for me] we use ABCDE all the way to F because addresses cap their lettering at F*/
         i++;
     }
 
-    int32_t right = i -1;
+    /* Do a reversal with the given value by making the highest number and the lowest number swap places until
+    * they meet in the middle and are correctlyy placed */
+    int32_t right = i-1;
+    while(left < right){             
+        char tmp = buf[left];      /* set a temp char to not overwrite & lose buf[left] when buf[right] has to become buf[left] */
 
-    // reversal
-    while(left < right){
-        char tmp = buf[left];
-        buf[left] = buf[right];
-        buf[right] = tmp;
-        left++;right--;
-        
+        buf[left] = buf[right];          /* current higher value swaps places with the current lowest value */ 
+        buf[right] = tmp;                /* buf[right] becomes buf[left] */
+
+        left++;right--;                
     }
 
-    if(i == 0){ buf[i]= '0';i++;}
+    /* since we're avoiding main loop when there's not an int given or a 0, we need to sill place a zero to avoid exiting program and increase 
+     * the counter to place the null terminator if/when needed */
+    if(i == 0){
+        buf[i]= '0';
+        i++;
+    }
     
-    buf[i] = '\0';
+    buf[i] = '\0'; /* place null terminator at the end */
 
 }
--- src/kernel/itoa.h
+++ src/kernel/itoa.h
@@ -2,7 +2,9 @@
 #define ITOA_H
 #include <stdint.h>
 
-/* buf must be at least 33 bytes (32 binary digits + '\0') */
+/* turns unsigned integer into a string using a given base
+ *
+ * buf must be at least 33 bytes (32 binary digits + '\0') */
 void utoa(uint32_t val, char *buf, int base);
 
-#endif
+#endif 
