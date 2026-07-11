6b7c0b3003c006fc1e496a102e9eba0060be7450
bcd532-laptop <bcd532@outerspace.com>
2026-07-11 10:37:06 -0400

    kmain.c: added some tests to check kprintf display
    they worked!
    
    took me multiple days to understand! but it works! and it was extremely
    easy! i'm actually very ashamed of myself!

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -7,12 +7,23 @@
 
 
 #define VGA_BUFFER ((volatile uint16_t *)0xb8000)
-char s[] = "bro";
+char dbg_S[] = "TEST";
+char dbg_C = 'X';
+uint32_t dbg_16b = 255;
+uint32_t dbg_uint = 586;
+
+
+
 void kmain(void) {
     KCONSOLE_VGA_INIT(); // initiate VGA console for writes
     KCONSOLE_VGA_WRITE("ok"); // ok
-    kprintf("hello %s\n", s);
+    kprintf("OUTPUT CHAR: %c\n", dbg_C);
+    kprintf("OUTPUT STRING: %s\n", dbg_S);
+    kprintf("OUTPUT HEX ADDRESS: %x\n", dbg_16b);
+    kprintf("OUTPUT UINT32: %u\n", dbg_uint);
+    kprintf("OUTPUT: %%");
     
+    KCONSOLE_VGA_SETCURSOR(9,9);
     KCONSOLE_VGA_WRITE("OK")    ;
     for (;;){}
 }
