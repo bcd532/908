7637cb82fd76598c26443c1c0d2b039ec2ec1269
bcd532-laptop <bcd532@outerspace.com>
2026-07-11 09:33:15 -0400

    kmain.c: added a test string to test kprintf.c

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -3,11 +3,16 @@
 #include "astdlib.h"
 #include "debugutils.h"
 #include "itoa.h"
+#include "kprintf.h"
+
 
 #define VGA_BUFFER ((volatile uint16_t *)0xb8000)
-
+char s[] = "bro";
 void kmain(void) {
     KCONSOLE_VGA_INIT(); // initiate VGA console for writes
     KCONSOLE_VGA_WRITE("ok"); // ok
+    kprintf("hello %s\n", s);
+    
+    KCONSOLE_VGA_WRITE("OK")    ;
     for (;;){}
 }
