59e40c8682f94b056cfc0fb2f322f9a16c66baff
bcd532 <yakud92@gmail.com>
2026-07-12 00:02:39 -0400

    interrupts.c: added a print for showing exceptions

--- src/kernel/interrupts.c
+++ src/kernel/interrupts.c
@@ -1,8 +1,12 @@
 #include "interrupts.h"
+#include "kvgacon.h"
+
 
 __attribute__((noreturn))
 void exception_handler(void);
 void exception_handler(){
+    KCONSOLE_VGA_WRITE("!! EXCEPTION - HALTED");
     __asm__ volatile ("cli; hlt");
     for(;;){};
 }
+
--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -4,12 +4,17 @@
 #include "debugutils.h"
 #include "itoa.h"
 #include "kvgaprintf.h"
+#include "idt.h"
 #include "advm.h"
 
 #define VGA_BUFFER ((volatile uint16_t *)0xb8000)
 
 void kmain(void) {
+    idt_init();
     KCONSOLE_VGA_INIT();
-    KCONSOLE_VGA_WRITE("[ OK ] ");
+    kvgaprintf("[ OK ]\n")
+    kvgaprintf("b4 exception\n");
+    __asm__ volatile ("int 32");
+    kvgaprintf("after (should not show)\n");
     for (;;){}
     }
