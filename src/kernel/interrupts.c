e706145d9947f3f80afad2d1c4de4f598862c64c
bcd532-laptop <bcd532@outerspace.com>
2026-07-13 03:24:33 -0400

    interrupts.c: changed exception msg

--- src/kernel/interrupts.c
+++ src/kernel/interrupts.c
@@ -5,7 +5,7 @@
 __attribute__((noreturn))
 void exception_handler(void);
 void exception_handler(){
-    KCONSOLE_VGA_WRITE("!! EXCEPTION - HALTED");
+    KCONSOLE_VGA_WRITE("!! EXCEPTION HALT !!");
     __asm__ volatile ("cli; hlt");
     for(;;){};
 }
