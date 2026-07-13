78922e8cb29f2517588aceb5b79eb1c57848aba8
bcd532-laptop <bcd532@outerspace.com>
2026-07-13 03:27:20 -0400

    NEW pic: added pic remapping to pair with keyboard io

---
+++ src/kernel/pic.c
@@ -0,0 +1,16 @@
+#include "io.h"
+#include <stdint.h>
+
+void pic_remap(void){
+    uint8_t mask = 0xFD;
+    outb(0x20, 0x11); /* begin initialization icw1 ->  command port*/
+        outb(0xA0, 0x11);
+    outb(0x21, 0x20); /* vector offset icw2 -> data port */
+        outb(0xA1, 0x28);
+    outb(0x21, 0x04); /* tells two chips how they're connected icw3 -> data port */
+        outb(0xA1, 0x02);
+    outb(0x21, 0x01); /* bit 0 */
+        outb(0xA1, 0x01);
+    outb(0x21, mask);
+        outb(0xA1, mask);
+}
---
+++ src/kernel/pic.h
@@ -0,0 +1,5 @@
+#ifndef PIC_H
+#define PIC_H 
+
+void pic_remap(void);
+#endif
