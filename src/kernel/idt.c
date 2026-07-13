9c7ea921dd96d751f0a371a26a6962c8be941856
bcd532-laptop <bcd532@outerspace.com>
2026-07-13 08:10:28 -0400

    idt.c: comment corrections

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -11,12 +11,12 @@ typedef struct {
 } __attribute__((packed)) idtr_t;
 
 /* Initialize the IDTR statically to avoid it sitting on the temporary function stack (prevents stack corruption, undefined behavior
- * and gurantees absolte memory reference for the assmebler) */
+ * and guarantees absolute memory reference for the assembler) */
 static idtr_t idtr; 
 
 /**/ 
 __attribute__((aligned(0x10))) /* forces the array to land on a 16-byte boundary to avoid cache line splits */
-static struct idt_entry_t idt[256]; /* init 256 arrays of idt gates */
+static struct idt_entry_t idt[256]; /* init 256 idt gates */
 
 
 void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags){
@@ -27,13 +27,13 @@ void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags){
     descriptor->selector                = 0x08;                              /* sets the selector to the code segment 0x08 */
     descriptor->zero_slot               = 0;                                 /* sets the zero slot */
     descriptor->attributes              = flags;                             /* sets the attributes to the given flags*/
-    descriptor->offset_s16e31           = ((uint32_t)isr >> 16)& 0xFFFF;     /* sets offset 2 (bits 16->31) to the given ISR's lower 32 bits */
+    descriptor->offset_s16e31           = ((uint32_t)isr >> 16)& 0xFFFF;     /* sets offset 2 (bits 16->31) to the given ISR's upper 16 bits */
 }
 
 
 
 void idt_init(){
-    idtr.limit = sizeof(idt) -1; /* sets the size of the idt descriptor to 31bits */
+    idtr.limit = sizeof(idt) -1; /* table size in bytes minus 1*/
     idtr.base = (uint32_t)&idt;  /* sets the address to the idt descriptor */
     for (int v = 0; v < 32; v++) idt_set_descriptor(v, isr_stub_table[v], 0x8E);
     __asm__ volatile( "lidt %0"
