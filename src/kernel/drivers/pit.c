12421c1550814ed666981a0203da4075d10fe941
bcd532 <yakud92@gmail.com>
2026-07-20 21:52:37 -0400

    pit.c: fixed overwrite contradiction when 65536 guard activates, also removed magic numbering

--- src/kernel/drivers/pit.c
+++ src/kernel/drivers/pit.c
@@ -4,7 +4,7 @@
 #include <cpu/interrupts.h>
 
 #define CLOCKFREQ 1193182
-
+#define TRUEMAX 65536
 static volatile uint32_t ticks;
 
 uint32_t pit_ticks(void){
@@ -19,13 +19,14 @@ void pit_ih(struct interrupt_frame* frame){
 }
 
 void pit_init(uint32_t hz){
+    if (hz == 0) return;
+
     // get 16-bit divisor
     uint32_t div = CLOCKFREQ / hz;
-    if (div == 0) return;
 
     // handle hardware limits and ensure the divisor fits in 16 bits    
-    if (div > 65535) div = 0;
-    if (div < 1) div = 1;
+    if (div > TRUEMAX) div = 0;
+    if (div == 0) return;
 
     // set PIT up with [SET MODE 3 | CHANNEL 0 | ACCESS LO/HI]
     outb(0x43, 0x36);
